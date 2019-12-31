c405b96832653a5969888500baa268a6f259952b
Joel Gainey <jwgainey@hotmail.com>
2019-12-31 13:50:36 -0500

    condenced for loop into another function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
 function randomRGB() {
    return Math.floor(Math.random() * 256 );
 }
 
+function randomColor() {
+    var color = 'rgb(';
+    color += randomRGB() + ',';
+    color += randomRGB() + ',';
+    color += randomRGB() + ')';
+    return color;
+}
+
 for (var i = 1; i <= 10; i +=1) {
-red = randomRGB();
-green = randomRGB();
-blue = randomRGB();
-rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+rgbColor = randomColor();
 html += '<div style="background-color:' + rgbColor + '"></div>';
 }
 
